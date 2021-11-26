c681ea51a48a208518d7aee90ec914d674263a5c
kaue <kauezinho_naruto@hotmail.com>
2021-11-25 22:55:31 -0300

    ProjetoRpgApi

---
+++ ScriptGeral.sql
@@ -0,0 +1,266 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Personagens] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] nvarchar(max) NULL,
+    [PontosVida] int NOT NULL,
+    [Forca] int NOT NULL,
+    [Defesa] int NOT NULL,
+    [Inteligencia] int NOT NULL,
+    [Classe] int NOT NULL,
+    CONSTRAINT [PK_Personagens] PRIMARY KEY ([Id])
+);
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Classe', N'Defesa', N'Forca', N'Inteligencia', N'Nome', N'PontosVida') AND [object_id] = OBJECT_ID(N'[Personagens]'))
+    SET IDENTITY_INSERT [Personagens] ON;
+INSERT INTO [Personagens] ([Id], [Classe], [Defesa], [Forca], [Inteligencia], [Nome], [PontosVida])
+VALUES (1, 0, 10, 10, 10, N'Frodo', 100),
+(2, 1, 25, 15, 30, N'Sam', 100),
+(3, 3, 21, 18, 35, N'Galadriel', 100),
+(4, 2, 18, 18, 37, N'Gandalf', 100),
+(5, 1, 17, 20, 31, N'Hobbit', 100),
+(6, 3, 13, 21, 34, N'Celeborn', 100),
+(7, 2, 11, 25, 35, N'Radagast', 100);
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Classe', N'Defesa', N'Forca', N'Inteligencia', N'Nome', N'PontosVida') AND [object_id] = OBJECT_ID(N'[Personagens]'))
+    SET IDENTITY_INSERT [Personagens] OFF;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20210831013743_MigracaoInicial', N'5.0.9');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Armas] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] nvarchar(max) NULL,
+    [Dano] int NOT NULL,
+    CONSTRAINT [PK_Armas] PRIMARY KEY ([Id])
+);
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Dano', N'Nome') AND [object_id] = OBJECT_ID(N'[Armas]'))
+    SET IDENTITY_INSERT [Armas] ON;
+INSERT INTO [Armas] ([Id], [Dano], [Nome])
+VALUES (1, 25, N'Espada');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Dano', N'Nome') AND [object_id] = OBJECT_ID(N'[Armas]'))
+    SET IDENTITY_INSERT [Armas] OFF;
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Dano', N'Nome') AND [object_id] = OBJECT_ID(N'[Armas]'))
+    SET IDENTITY_INSERT [Armas] ON;
+INSERT INTO [Armas] ([Id], [Dano], [Nome])
+VALUES (2, 30, N'Machado');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Dano', N'Nome') AND [object_id] = OBJECT_ID(N'[Armas]'))
+    SET IDENTITY_INSERT [Armas] OFF;
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Dano', N'Nome') AND [object_id] = OBJECT_ID(N'[Armas]'))
+    SET IDENTITY_INSERT [Armas] ON;
+INSERT INTO [Armas] ([Id], [Dano], [Nome])
+VALUES (3, 20, N'Crucifixo');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Dano', N'Nome') AND [object_id] = OBJECT_ID(N'[Armas]'))
+    SET IDENTITY_INSERT [Armas] OFF;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20210918143744_MigracaoArma', N'5.0.9');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+ALTER TABLE [Personagens] ADD [FotoPersonagem] varbinary(max) NULL;
+GO
+
+ALTER TABLE [Personagens] ADD [UsuarioId] int NULL;
+GO
+
+CREATE TABLE [Usuarios] (
+    [Id] int NOT NULL IDENTITY,
+    [Username] nvarchar(max) NULL,
+    [PasswordHash] varbinary(max) NULL,
+    [PasswordSalt] varbinary(max) NULL,
+    [Foto] varbinary(max) NULL,
+    [Latitude] nvarchar(max) NULL,
+    [Longitude] nvarchar(max) NULL,
+    [DataAcesso] datetime2 NULL,
+    CONSTRAINT [PK_Usuarios] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE INDEX [IX_Personagens_UsuarioId] ON [Personagens] ([UsuarioId]);
+GO
+
+ALTER TABLE [Personagens] ADD CONSTRAINT [FK_Personagens_Usuarios_UsuarioId] FOREIGN KEY ([UsuarioId]) REFERENCES [Usuarios] ([Id]) ON DELETE NO ACTION;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20210921013451_MigracaoUsuario', N'5.0.9');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+ALTER TABLE [Armas] ADD [PersonagemId] int NOT NULL DEFAULT 0;
+GO
+
+UPDATE [Armas] SET [PersonagemId] = 1
+WHERE [Id] = 1;
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [Armas] SET [PersonagemId] = 2
+WHERE [Id] = 2;
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [Armas] SET [PersonagemId] = 3
+WHERE [Id] = 3;
+SELECT @@ROWCOUNT;
+
+GO
+
+CREATE UNIQUE INDEX [IX_Armas_PersonagemId] ON [Armas] ([PersonagemId]);
+GO
+
+ALTER TABLE [Armas] ADD CONSTRAINT [FK_Armas_Personagens_PersonagemId] FOREIGN KEY ([PersonagemId]) REFERENCES [Personagens] ([Id]) ON DELETE CASCADE;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20210927231124_MigracaoAtualizacaoArmaPersonagem', N'5.0.9');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Habilidades] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] nvarchar(max) NULL,
+    [Dano] int NOT NULL,
+    CONSTRAINT [PK_Habilidades] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [PersonagemHabilidades] (
+    [PersonagemId] int NOT NULL,
+    [HabilidadeId] int NOT NULL,
+    CONSTRAINT [PK_PersonagemHabilidades] PRIMARY KEY ([PersonagemId], [HabilidadeId]),
+    CONSTRAINT [FK_PersonagemHabilidades_Habilidades_HabilidadeId] FOREIGN KEY ([HabilidadeId]) REFERENCES [Habilidades] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_PersonagemHabilidades_Personagens_PersonagemId] FOREIGN KEY ([PersonagemId]) REFERENCES [Personagens] ([Id]) ON DELETE CASCADE
+);
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Dano', N'Nome') AND [object_id] = OBJECT_ID(N'[Habilidades]'))
+    SET IDENTITY_INSERT [Habilidades] ON;
+INSERT INTO [Habilidades] ([Id], [Dano], [Nome])
+VALUES (1, 29, N'Bola de fogo');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Dano', N'Nome') AND [object_id] = OBJECT_ID(N'[Habilidades]'))
+    SET IDENTITY_INSERT [Habilidades] OFF;
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Dano', N'Nome') AND [object_id] = OBJECT_ID(N'[Habilidades]'))
+    SET IDENTITY_INSERT [Habilidades] ON;
+INSERT INTO [Habilidades] ([Id], [Dano], [Nome])
+VALUES (2, 41, N'Portal');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Dano', N'Nome') AND [object_id] = OBJECT_ID(N'[Habilidades]'))
+    SET IDENTITY_INSERT [Habilidades] OFF;
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Dano', N'Nome') AND [object_id] = OBJECT_ID(N'[Habilidades]'))
+    SET IDENTITY_INSERT [Habilidades] ON;
+INSERT INTO [Habilidades] ([Id], [Dano], [Nome])
+VALUES (3, 37, N'Congelar');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Dano', N'Nome') AND [object_id] = OBJECT_ID(N'[Habilidades]'))
+    SET IDENTITY_INSERT [Habilidades] OFF;
+GO
+
+CREATE INDEX [IX_PersonagemHabilidades_HabilidadeId] ON [PersonagemHabilidades] ([HabilidadeId]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20210928011556_MigracaoPersonagemHabilidade', N'5.0.9');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+ALTER TABLE [Usuarios] ADD [Perfil] nvarchar(max) NOT NULL DEFAULT N'Jogador';
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20211018191627_MigracaoPerfil', N'5.0.9');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+DECLARE @var0 sysname;
+SELECT @var0 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Usuarios]') AND [c].[name] = N'Perfil');
+IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [Usuarios] DROP CONSTRAINT [' + @var0 + '];');
+ALTER TABLE [Usuarios] ALTER COLUMN [Perfil] nvarchar(max) NULL;
+ALTER TABLE [Usuarios] ADD DEFAULT N'Jogador' FOR [Perfil];
+GO
+
+ALTER TABLE [Personagens] ADD [Derrotas] int NOT NULL DEFAULT 0;
+GO
+
+ALTER TABLE [Personagens] ADD [Disputas] int NOT NULL DEFAULT 0;
+GO
+
+ALTER TABLE [Personagens] ADD [Vitorias] int NOT NULL DEFAULT 0;
+GO
+
+CREATE TABLE [Disputas] (
+    [Id] int NOT NULL IDENTITY,
+    [DataDisputa] datetime2 NULL,
+    [AtacanteId] int NOT NULL,
+    [OponenteId] int NOT NULL,
+    [Narracao] nvarchar(max) NULL,
+    CONSTRAINT [PK_Disputas] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20211025190424_MigracaoDisputas', N'5.0.9');
+GO
+
+COMMIT;
+GO
+
